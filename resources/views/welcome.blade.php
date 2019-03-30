13127d71d454d6caac713087a3e760f9714d709f
Mambo <haizul.harudin@gmail.com>
2019-03-30 20:58:46 +0800

    initial upload

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,136 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<html lang="en">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="UTF-8">
+    <title>Malaysia Timeline </title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+<body>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <nav class="navbar navbar-expand-sm bg-warning">
+        <div class="container-fluid">
+            <a href="#" class="navbar-brand" style="text-decoration: none;color: #000"><b>Malaysia</b>timeline</a>
+        </div>
+    </nav>
+    <header>
+    </header>
+    <section class="promo-details py-5" id="promo">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="right-color col-md-6 py-5 px-5">
+                    <h2><b>Membeli sambil beramal Bersama <br><i><small>@The Pondok Project</small></i></b></h2>
+                    <hr width="50%">
+                    <p>Program yang buat pertama kalinya diadakan hujung minggu menawarkan lebih dari 3,000 barangan terpakai termasuk pakaian lelaki dan wanita, beg tangan, kasut, aksesori dan lain-lain.</p>
+                    <p>Jualan amal ini diadakan bagi mengumpulkan dana untuk pembinaan Pondok Maahad Tahfiz Saidina Uthman Ibnu Affan, Pokok Sena, di Kedah Darul Aman.</p>
+                    <p>Jom!! Kita membeli sambil menderma di jualan amal yang akan diadakan pada 30 - 31 mac 2019 bertempat di Royal Widad Residence @ UTMKL, Kuala Lumpur. Semoga Pondok Maahad Tahfiz Saidina Uthman Ibnu Affan, Pokok Sena, Kedah dapat dibina. Biar saham akhirat kita kongsi bersama.</p>
+                    <p>Doa Keselamatan Dan Murah Rezeki Rabbij'al Haadzaa Baladan Aaminan Warzuq Ahlahuu Minassamaraati Man Aamana Minhum Billaahi Walyaumil Aakhir.</p>
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+                    <!-- <p>Follow Social Media Thepondokproject;</p>-->
+                    <div class="social-media">
+                        <a href="https://www.facebook.com/The-Pondok-Project-961303770924958/" style="text-decoration: none;"><img src="images/facebook.svg" alt="" width="40px"> The Project Pondok
+                        </a>
+                        <a href="#" style="text-decoration: none;"><img src="../images/instagram.svg" alt="" width="40px">@TheProjectPondok
+                        </a>
+                    </div>
+                    <div class="pquote">
+                        <blockquote>
+                            <p><i>Allah SWT berfirman yang bermaksud: “Katakanlah! Sesungguhnya, Tuhanku melapangkan rezeki dan menyekatnya kepada siapa yang Dia kehendaki di antara hamba-Nya; Dan apa sahaja yang kamu berikan, Allah akan menggantinya dan Dialah Pemberi Rezeki yang terbaik.” </i></p>
+                            <footer class="blockquote-footer"><b>(Surah Saba’, ayat 34)</b></cite></footer>
+                    </div>
                 </div>
-            @endif
+                <div class="col-md-6 poster">
+                    <img src="images/poster/IMG-20190325-WA0006.jpg" class="rounded" style="width: 90%">
+                    <img src="images/poster/preloved.jpg" class="rounded" style="width: 90%;padding-top:5px;">
+                </div>
+            </div>
+    </section>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+
+    <section id="events-list" class="my-2">
+        <h2 class="titl py-5">Minggu ini</h2>
+        <div class="container">
+            <div class="card text-center my-2">
+                <div class="r-image-card">
+                    <img src="images/poster/preloved.jpg" alt="" class="card-img-top">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Jualan Amal</h5>
+                    <p class="card-text">Salam warga Malaysia yang berhati penderma, Jom!! Kita membeli sambil menderma di jualan amal yang akan diadakan pada 30 - 31 mac 2019 bertempat di Royal Widad Residence @ UTMKL, Kuala Lumpur. Semoga Pondok Maahad Tahfiz Saidina Uthman Ibnu Affan, Pokok Sena, Kedah dapat dibina. Biar saham akhirat kita kongsi bersama.</p>
+                    <a href="https://www.facebook.com/The-Pondok-Project-961303770924958/" class="btn btn-outline-primary">Facebook</a>
+                </div>
+                <div class="card-footer text-muted">
+                    1 hari lagi
+                </div>
+            </div>
+
+            <div class="card text-center my-5">
+                <div class="r-image-card">
+                    <img src="images/poster/sampah-menyampah.jpg" alt="" class="card-img-top">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Sampah,Menyampah and Friends' EARTH Weekend
+                    </h5>
+                    <p class="card-text">
+                        Datang menyertai kami dalam merayakan Bumi kita selama dua hari perkongsian, pensampelan dan menjalani kehidupan yang mampan! </p>
+                    <a href="https://www.facebook.com/events/568055310330875/" class="btn btn-outline-primary">Facebook</a>
+                </div>
+                <div class="card-footer text-muted">
+                    14 hari lagi
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+
+
+            </div>
+        </div>
+    </section>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLongTitle">@The Pondok Project <br>Jom Ke Jualan Amal</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="text-body">
+                        <p>Tempat: <b>Royal Widad Residence @ UTMKL, Kuala Lumpur.</b> </p>
+                        <p>Tarikh: <b>30 - 31 mac 2019</b> </p>
+                        <p>Berkenaan: <b>Pembinaan Pondok Maahad Tahfiz Saidina Uthman Ibnu Affan, Pokok Sena, di Kedah Darul Aman.</b></p>
+                    </div>
                 </div>
             </div>
         </div>
-    </body>
+    </div>
+
+    <!-- Footer -->
+    <footer class="page-footer font-small blue pt-4">
+        <!-- Copyright -->
+        <div class="footer-copyright text-center py-3">
+            <div id="webs" class="fsmall"></div>
+        </div>
+        <!-- Copyright -->
+
+    </footer>
+    <!-- Footer -->
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script>
+        $(window).on('load', function() {
+            $('#exampleModalCenter').modal('show');
+        });
+        // Time
+        var dn = new Date();
+        var year = dn.getFullYear();
+        document.getElementById('webs').innerHTML = location.hostname + '<br>' + year + ' ' + 'Copyright';
+    </script>
+</body>
+
 </html>
